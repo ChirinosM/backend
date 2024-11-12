35003f908336fd70cf39c5c968890e078c3d15a1
MarielaC <mchirinos@trafficlight.mx>
2024-11-12 13:48:50 -0600

    dad

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const mysql = require('mysql2');
 
+let valoresCenturion = {};
+
 // Configuración de CORS y JSON
 app.use(cors());
 app.use(express.json());
@@ -11,7 +13,7 @@ app.use(express.json());
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '', // Asegúrate de que esta sea la contraseña correcta
+    password: 'Centurion24.', // Asegúrate de que esta sea la contraseña correcta
     database: 'BD_CENTURION_PRUEBAS',
 });
 
@@ -20,17 +22,17 @@ const promisePool = pool.promise();
 
 // Ruta POST para la autenticación de sesión
 app.post('/sesion', async (req, res) => {
-    const { usuario, password } = req.body;
+    var { usuario, password } = req.body;
     console.log('Datos recibidos:', { usuario, password });
 
     try {
-        const [rows] = await promisePool.query(
-            'SELECT * FROM USUARIOS WHERE EMAIL = ?',
+        var [rows] = await promisePool.query('SELECT USUARIOS.EMAIL, ROLES.ROL, CIUDADES.NAME_CITY, PROYECTOS.NAME_PROY FROM USUARIOS LEFT OUTER JOIN ROLES ON (ROLES.ID = USUARIOS.ID) LEFT OUTER JOIN USUARIOS_REL_PROYECTOS ON (USUARIOS_REL_PROYECTOS.ID_USER = USUARIOS.ID) LEFT OUTER JOIN PROYECTOS ON (PROYECTOS.ID = USUARIOS_REL_PROYECTOS.ID_PROY) LEFT OUTER JOIN CIUDADES ON (CIUDADES.ID = PROYECTOS.ID_CITY) WHERE EMAIL = ? ',
             [usuario]
         );
 
         if (rows.length > 0) {
-            res.status(200).send('Usuario ingresado');
+            console.log('Datos enviados:', rows);
+            res.status(200).json(rows);
             // res.redirect(/perfil?usuario=${user}&autenticado=true);
         } else {
             res.status(401).send('Usuario o contraseña incorrectos');
@@ -44,6 +46,43 @@ app.post('/sesion', async (req, res) => {
     }
 });
 
+app.post('/Centurion', async (req, res) => {
+    var { Email, Rol, Proyecto } = req.body;
+    console.log('Datos recibidos:', { Email, Rol, Proyecto });
+
+    try {
+        if (Rol === 'CenturionAdmi') {
+            Rol = 'Administrador';
+        } else if (Rol === 'CenturionOper')
+            Rol = 'Operador';
+        else Rol = 'Visit';
+
+        var [rows] = await promisePool.query('SELECT USUARIOS.EMAIL, ROLES.ROL, CIUDADES.NAME_CITY, PROYECTOS.NAME_PROY FROM USUARIOS LEFT OUTER JOIN ROLES ON(ROLES.ID = USUARIOS.ID) LEFT OUTER JOIN USUARIOS_REL_PROYECTOS ON(USUARIOS_REL_PROYECTOS.ID_USER = USUARIOS.ID) LEFT OUTER JOIN PROYECTOS ON(PROYECTOS.ID = USUARIOS_REL_PROYECTOS.ID_PROY) LEFT OUTER JOIN CIUDADES ON(CIUDADES.ID = PROYECTOS.ID_CITY) WHERE USUARIOS.EMAIL = ? ',
+            [Email]
+        );
+        console.log('Datos BD: ', rows);
+
+        if (rows.length > 0) {
+            valoresCenturion = rows;
+            res.status(200).json(rows);
+            // res.redirect(/perfil?usuario=${user}&autenticado=true);
+        } else {
+            res.status(401).send('Usuario o contraseña incorrectos');
+        }
+    } catch (err) {
+        console.error('Error en la consulta:', err);  // Aquí se imprime el error en la consola
+        res.status(500).send({
+            error: 'Error en el servidor',
+            details: err.message || 'Sin detalles del error'  // Agregar detalles del error
+        });
+    }
+});
+
+app.post('/RecibeCenturion', async (req, res) => {
+    console.log('Recibiendo los datos de Centurion: ', valoresCenturion);
+
+    res.status(200).json(valoresCenturion);
+});
 
 // Iniciar el servidor Express
 app.listen(3010, () => {
